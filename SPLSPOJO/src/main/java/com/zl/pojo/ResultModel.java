446c617c8dc2ee895c98a81b628c003712359922
15873594244 <474227281@qq.com>
2019-08-15 19:45:07 +0800

    json返回模型

---
+++ SPLSPOJO/src/main/java/com/zl/pojo/ResultModel.java
@@ -0,0 +1,26 @@
+package com.zl.pojo;
+
+public class ResultModel {
+	private boolean success=false;
+	private Object data;
+	private String message;
+	public boolean isSuccess() {
+		return success;
+	}
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+	public Object getData() {
+		return data;
+	}
+	public void setData(Object data) {
+		this.data = data;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
